de2a5edb721b21bcbb8d9729f543d5a7e6edcba5
tschmi5 <1timetyler@gmail.com>
2019-04-24 21:10:32 -0400

    Both Charts working

--- SteadyState/script.js
+++ SteadyState/script.js
@@ -5,17 +5,11 @@ function graph()
   
   // alert(vals);
 
-  var rolls = [];
-  var rollNum = [];
-  var data = [];
+  var rolls = [1000];
+  var rollNum = [1000];
+  var avgs = [1000];
   var total = 0;
-  for (var i = 0; i < 1000; i++){
-      rollNum.push((i+1));
-      rolls.push(Math.floor(Math.random() * (+7 - +1)) + 1);
-      total+= rolls[i];
-      data.push(total/(i+1));
-      console.log(i + " " + rolls[i] + " " + data[i]);
-  }
+  
   //rollNum.push(1000);
 
 
@@ -23,38 +17,51 @@ function graph()
 
 
   var ctx = document.getElementById("myChart");
-  var myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-      labels: rollNum,
+
+  var data = {
+    labels: rollNum,
       datasets: [
         { 
-            data: data,
+            data: [],
             label: "data",
             borderColor: 'rgba(255, 99, 132, 1)',
             backgroundColor: 'rgba(255, 99, 132, 0.5)',
             // fill: false
         }
       ]
-    },
-    options: {
-      scales: {
-        xAxes: [{
-          ticks: {
-            maxTicksLimit: 10,
-            
-            stepSize: 100
-          }
-        }],
-        yAxes: [{
-          ticks: {
-            min: 1,
-            max: 6,
-            stepSize: 0.5
-          }
+    };
+  }
+  var options = {
+    scales: {
+      xAxes: [{
+        ticks: {
+          maxTicksLimit: 10,
           
-        }]
-      }
+          stepSize: 100
+        }
+      }],
+      yAxes: [{
+        ticks: {
+          min: 1,
+          max: 6,
+          stepSize: 0.5
+        }
+        
+      }]
     }
-  });
+  };
+  var myChart = new Chart(ctx).Line(data,options);
+
+  setInterval(function(){
+    setData(data.datasets[0].data);
+    var myChart = new Chart(ctx).Line(data,options);
+  },2000);
+
+  for (var i = 0; i < 1000; i++){
+    rollNum.push((i+1));
+    rolls.push(Math.floor(Math.random() * (+7 - +1)) + 1);
+    total+= rolls[i];
+    data[i]= (total/(i+1));
+    console.log(i + " " + rolls[i] + " " + data[i]);
+}
 }
